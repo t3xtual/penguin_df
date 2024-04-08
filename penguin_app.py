b0fd6801a5a2ee785c9e84f1fbe75a937dc2fe72
t3xtual <166205876+t3xtual@users.noreply.github.com>
2024-04-08 13:48:16 +0400

    Update penguin_app.py

--- penguin_app.py
+++ penguin_app.py
@@ -1,8 +1,6 @@
 # Importing the necessary libraries.
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import streamlit as st
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
